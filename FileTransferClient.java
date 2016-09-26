8c8c64dfc692da988f6638c37b736bd55bdf3170
DanGravel <dan0gravel@gmail.com>
2016-09-26 15:11:43 -0400

    removed white space and refactored IntermediateHost.java to be more self documenting

--- FileTransferClient.java
+++ FileTransferClient.java
@@ -10,9 +10,6 @@ public class FileTransferClient extends Host{
 	
 	public FileTransferClient() {
 		try {
-	         // Construct a datagram socket and bind it to any available 
-	         // port on the local host machine. This socket will be used to
-	         // send UDP Datagram packets.
 			sendReceiveSocket = new DatagramSocket();
 		} catch (SocketException se) {
 			se.printStackTrace();
@@ -20,24 +17,21 @@ public class FileTransferClient extends Host{
 		}
 	}
 
-	public void sendAndReceive() {
+	public void sendAndReceive(String message) {
 		for(int x = 0; x < 11; x++) { //Send 11 packets in total
-		      String message = "My fake pin is: 12345678";
+		      String message = message;
 		      byte readOrWrite[] = (x%2<1) ? read() : write (); //If even request make array {0, 1}, else {0,2}
 		      byte finalMsg[] = arrayCombiner(readOrWrite, message); // Combine all segments of message to make final message
 		      if(x == 10) finalMsg = new byte[] { 0, 0, 0, 0};    //Invalid format, sent to fail	
 		      sendaPacket(finalMsg, INTERMEDIATE_PORT, sendReceiveSocket, "Client");              
 		      receiveaPacket("Client", sendReceiveSocket);
-		     
-		   
-			 
 			}
-		      sendReceiveSocket.close();
+		    sendReceiveSocket.close();
 	}
 	
 	private byte[] arrayCombiner(byte readOrWrite[], String message) {
 		  byte msg[] = message.getBytes();
-		  byte seperator[] = new byte[] {0x00}; //zeroByte();
+		  byte seperator[] = new byte[] {0}; //zeroByte();
 		  byte mode[] = "ascii".getBytes();
 		  ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
 		  try {
@@ -56,7 +50,7 @@ public class FileTransferClient extends Host{
 	    * @return 	Returns a byte array of {0, 1} which corresponds to read request
 	    */
 	   private byte[]  read() {
-	      return new byte[] {0x00,0x01};
+	      return new byte[] {0,1};
 	   } 
 	   
 	   /**
@@ -64,7 +58,7 @@ public class FileTransferClient extends Host{
 	    * @return	Returns a byte array containing {0, 2} which corresponds to write request
 	    */
 	   private byte[] write() {
-	      return new byte[] {0x00,0x02};
+	      return new byte[] {0,2};
 	   }
 	   
 
