a4dba57c375f5ce79dd4d2792773099ee36f356a
Kshamina <kshamina.ghelani@carleton.ca>
2016-12-01 22:28:08 -0500

    normal()  reprompting again

--- src/IntermediateHost.java
+++ src/IntermediateHost.java
@@ -174,18 +174,14 @@ public class IntermediateHost extends Host {
 	}
 	
 	private void normal() {
-		while(!error) {
-			RequestType r = validate.validate(receiveFromClient(PACKET_SIZE).getData());
-			int clientPort = receivePacket.getPort();
-			sendToServer();
-			if(r == RequestType.WRITE) receiveFromServer(ACK_PACKET_SIZE);
-			else receiveFromServer(PACKET_SIZE);
-			int serverThreadPort = receivePacket.getPort();
-			sendToClient(clientPort);
-			
-			finishTransfer(r, clientPort, serverThreadPort);
-		}
-		
+		RequestType r = validate.validate(receiveFromClient(PACKET_SIZE).getData());
+		int clientPort = receivePacket.getPort();
+		sendToServer();
+		if(r == RequestType.WRITE) receiveFromServer(ACK_PACKET_SIZE);
+		else receiveFromServer(PACKET_SIZE);
+		int serverThreadPort = receivePacket.getPort();
+		sendToClient(clientPort);
+		finishTransfer(r, clientPort, serverThreadPort);		
 	}
 
 	private void losePacket() {
